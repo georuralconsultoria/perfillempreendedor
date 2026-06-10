979fb20cafe83ac961c4c9bc7a3b22480e1d3dff
tcfmrg <marcielsatinho@gmail.com>
2026-06-10 14:19:58 -0300

    Update config.js

--- config.js
+++ config.js
@@ -15,22 +15,6 @@ window.APP_CONFIG = {
   SUPABASE_URL: "https://rtllxyjtprhhaolrzkoa.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_CwQpgkbV4dEvtId_m92dJw_ztmlGmNy"
 };
-console.log("Dados que serão enviados:", resposta);
-
-const { error } = await supabaseClient
-  .from("respostas_perfil_empreendedor")
-  .insert([resposta]);
-
-if (error) {
-  console.error("ERRO COMPLETO DO SUPABASE:", error);
-
-  alert(
-    "Não foi possível enviar.\n\n" +
-    "Mensagem: " + error.message + "\n" +
-    "Código: " + (error.code || "não informado") + "\n" +
-    "Detalhes: " + (error.details || "não informado") + "\n" +
-    "Dica: " + (error.hint || "não informada")
-  );
 
   return;
 }
